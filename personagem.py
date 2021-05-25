1c1cb1a1350a22ab66f1606234bbb3ca3e1466f7
Matheus Rodrigues <matheusrodriguesoliveira273@gmail.com>
2021-05-25 17:22:51 -0300

    Criando classe Personagem
    Classe personagem

---
+++ personagem.py
@@ -0,0 +1,10 @@
+class Personagem:
+    def __init__(self):
+        self.forca = False
+        self.velocidade = False
+        self.inteligencia = False
+        self.sorte = False
+        self.escolha = False
+
+    def __str__(self):
+        return "Você escolheu" + (f" {self.escolha}")
