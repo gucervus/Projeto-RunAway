6519b8d26db96ee8e555be9ecf706e9480201c88
João <hlabeats@gmail.com>
2021-05-25 21:41:02 -0300

    Sala1
    iniciando a primeira sala

---
+++ CORES.py
@@ -0,0 +1,16 @@
+cores = {'limpa': '\033[m', 'azul': '\033[34;40m', 'fundoazul':'\033[7;36m', 'red':'\033[4;31m'}
+#print(cores)
+'''print('\033[4;31;40m-*-\033[m'*30)
+num=float(input('\033[7;30;44mDigite o num:' ))
+print('{}O valor {}digitado{} foi: {}{}{} e sua porção inteira é: {}'.format(cores['limpa'],cores['azul'], cores['limpa'],cores['red'],num,cores['limpa'], int(num)))'''
+
+
+'''30      black       preto          40
+31      red           vermelho   41
+32      green       verde         42
+33      yellow      amarelo    43
+34      blue          azul           44
+35      Magenta  Magenta  45
+36      cyan         ciano         46
+37      grey          cinza         47
+97      white        branco     107'''
