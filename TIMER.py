b1e4c79d1671f9a0b4d7b6571495c1ec498dc154
João <hlabeats@gmail.com>
2021-05-27 15:56:12 -0300

    Create TIMER.py

---
+++ TIMER.py
@@ -0,0 +1,21 @@
+import time
+
+class Timer:
+    def __init__(self, value = 0):
+        self.start = time.time()
+        self.set(value)
+
+    def set(self, value : float):
+        self.start = time.time() - value
+
+
+    def get(self) -> float:
+        return time.time() - self.start 
+    
+    def __str__(self):
+        value = self.get()
+        sec = int(value)
+        min = int(sec/60)
+        sec=int(value)
+        return str(min) +'m '+str(sec)+'s ' 
+        
