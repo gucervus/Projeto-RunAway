d4f726e851acda948ab755b6da2e035e5090d43a
Matheus Rodrigues <matheusrodriguesoliveira273@gmail.com>
2021-05-30 22:52:47 -0300

    Refatorando a sala 2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from relogio import Relógio
 from personagem import Personagem
 from primeiraSala import salaVermelha
@@ -15,6 +16,7 @@ tecla = pygame.mixer.Sound('teclado.ogg')
 pygame.mixer.music.load('trilhasuspensa.ogg')
 pygame.mixer.music.set_volume(0.1)
 pygame.mixer.music.play(-1)
+
 
 if (__name__ == "__main__"):
 
@@ -107,43 +109,4 @@ if (__name__ == "__main__"):
 
         # sala1.acao()
 
-    func = Funções(genero=gen)
-
-    tecla.play(-1)
-    print(' '*25, end='')
-    bemVindo = f"Bem vind{func.generos()} {nome}\n\n"
-    func.animation(bemVindo)
-    print(' '*20, end='')
-    atributo = '»»»» Escolha um atributo ««««\n\n\n\n'
-    func.animation(atributo)
-    tecla.stop()
-
-    sleep(1)
-    print("Qual atributo você escolhe? \n\n",
-          '[1] - Força\n',
-          '[2] - Velocidade\n',
-          '[3] - Inteligência\n',
-          '[4] - Sorte\n\n')
-    sleep(.5)
-
-    opcao = int(input('»» '))
-    print()
-
-    if opcao == 1:
-        escolha = "Força"
-    elif opcao == 2:
-        escolha = "Velocidade"
-    elif opcao == 3:
-        escolha = 'Inteligência'
-    elif opcao == 4:
-        escolha = "Sorte"
-
-    personagem = Personagem(nome, alt, escolha)
-
-    salas = salaVermelha(nome=nome, altura=alt, escolha=escolha, genero=gen)
-    sala2 = salaBranca()
-    personagem.escolhaAtributo()
-
-    salas.acao()
-
-    sala2.acao()
+        sala2.acao()
