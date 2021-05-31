c440ca9907b57066f0f9643a0c2ae9593c8b5904
Matheus Rodrigues <matheusrodriguesoliveira273@gmail.com>
2021-05-31 19:50:07 -0300

    update game over

--- _time/clock.py
+++ _time/clock.py
@@ -16,26 +16,3 @@ class Relógio(Personagem):
 
     def __str__(self):
         return f'{self.minutos:02d}'
-
-
-# Parte de teste
-"""while fimdotempo == False:
-    print("---")
-    print("Ainda restam "+str(relogio)+" minutos")
-    print("")
-    print("Ações:")
-    print("1 - Tentar")
-    
-    print("0 - Sair do jogo")
-    opcao = input("Escolha sua ação:")
-    if(opcao == "1") and (relogio.minutos > 0):
-        relogio.corretempo(5)
-    if relogio.minutos <= 0:
-        fimdotempo = True   
-        print('ACABOU O TEMPO')
-        break
-        
-    elif(opcao == '0'):
-        print('Tente amanhã!')
-        break
-    """
--- main.py
+++ main.py
@@ -133,3 +133,4 @@ if (__name__ == "__main__"):
             break
         else:
             sala2.acao()
+            break
